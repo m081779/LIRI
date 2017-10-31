d739cfb666bb6862bb160c085d917e5a60158cfc
m081779 <marcoprincipio@hotmail.com>
2017-10-30 23:54:47 -0400

    fleshed out the readme file with some instructions on how to use the program

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # LIRI
+
+
+To return the last 20 of my tweets, at the command line enter:
+node liri my-tweets
+
+To return information about a song, at the command line enter:
+node liri spotify-this-song "song name here"
+
+To return information about a movie, at the command line enter:
+node liri movie-this "movie name here"
+
+To return information about the song specified in random.txt, at the command line enter:
+node do-what-it-says
--- keys.js
+++ keys.js
@@ -7,14 +7,3 @@ var twitterKeys = {
 }
 
 module.exports = twitterKeys;
-
-// Consumer Key (API Key)	a7GimwcAWuKGhWqA0tyZik4yb
-// Consumer Secret (API Secret)	EsDcrB6wG9zQIIs82iSsVCRIzMRGPrMFsJl4h5R9NDr59esOi1
-// Access Level	Read and write (modify app permissions)
-// Owner	test_acc1979
-// Owner ID	924748051297853445
-// Access Token	924748051297853445-QvQ5wmslXDE21ZH1n5DSsJfTcSdJkbS
-// Access Token Secret	y0Gddkwokg7lczmxrGV52fvldeZNEgwu8ziP6ivec3JbN
-// Access Level	Read and write
-// Owner	test_acc1979
-// Owner ID	924748051297853445
